496f1fcf957625747720a8caaf2c9d7886596200
Intan Sari Malia Dewi <145327324+IntanSariMaliaDewi@users.noreply.github.com>
2024-04-30 16:52:41 +0700

    Delete ecommerce_app directory

--- ecommerce_app/model/ProductModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ProductModel {
-    public function getAllProducts() {
-        // Di sini Anda akan memiliki logika untuk mengambil data produk dari database atau sumber data lainnya
-        // Sebagai contoh, kita akan mengembalikan array statis
-        return array("Product 1", "Product 2", "Product 3");
-    }
-}
-?>
--- ecommerce_app/model/SupplierModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-class SupplierModel {
-    public function getAllSuppliers() {
-        // Di sini Anda akan memiliki logika untuk mengambil data supplier dari database atau sumber data lainnya
-        // Sebagai contoh, kita akan mengembalikan array statis
-        return array("Supplier 1", "Supplier 2", "Supplier 3");
-    }
-}
-?>
